9e03b6792e1b7822899ebc919ad9f9a54aac1eb4
George Munganga <georgemunganga@gmail.com>
2026-08-10 09:59:10 +0200

    make document deduplication migration reversible

--- internal/database/migrations/00005_document_type_deduplication.sql
+++ internal/database/migrations/00005_document_type_deduplication.sql
@@ -1,6 +1,15 @@
+-- +goose Up
 ALTER TABLE documents
   DROP CONSTRAINT IF EXISTS documents_tenant_id_environment_source_application_source_r_key;
 
 ALTER TABLE documents
   ADD CONSTRAINT documents_source_document_hash_key
   UNIQUE (tenant_id, environment, source_application, source_reference, document_type, sha256_hex);
+
+-- +goose Down
+ALTER TABLE documents
+  DROP CONSTRAINT IF EXISTS documents_source_document_hash_key;
+
+ALTER TABLE documents
+  ADD CONSTRAINT documents_tenant_id_environment_source_application_source_r_key
+  UNIQUE (tenant_id, environment, source_application, source_reference, sha256_hex);
